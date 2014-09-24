6f4223a24062cb2fee161019e600ff89f0b997be
Faraz Fallahi <fffaraz@gmail.com>
2014-09-24 15:15:31 -0500

    start

---
+++ mynl/lab1.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int main(void)
+{
+    for(int i=0; i<10; i++)
+        printf("Hello World!\n");
+    return EXIT_SUCCESS;
+}
+
