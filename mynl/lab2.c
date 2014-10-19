26ac972a0abc9d7c09e0125b8adde7709c3da196
Faraz Fallahi <fffaraz@gmail.com>
2014-10-19 18:16:50 -0500

    lab2.c

--- mynl/lab2.c
+++ mynl/lab2.c
@@ -8,12 +8,97 @@
 #include <regex.h>
 #include <errno.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #define bool _Bool
 #define true (1)
 #define false (0)
 
 // this macro saves the cost of a function call if the first letters differ
 #define STREQ(a, b) (*(a) == *(b) && strcmp((a), (b)) == 0)
+
+//----------------------------------------------------------------------
+
+#define FILE_BUFFER_SIZE 512
+
+#define BUFF_SIZE_INIT 50
+#define BUFF_SIZE_INC  10
+
+//----------------------------------------------------------------------
+
+#define MY_FILE struct _MY_FILE
+
+struct _MY_FILE
+{
+    int fd;
+    int flags;
+    char buf[FILE_BUFFER_SIZE];
+    int size;
+    int pos;
+};
+
+MY_FILE my_stdin[] = { { STDIN_FILENO, 0, {}, 0, 0 } };
+
+int my_fgetc(MY_FILE* stream)
+{
+    if(stream->pos == stream->size)
+    {
+        stream->pos = 0;
+        stream->size = read(stream->fd, stream->buf, FILE_BUFFER_SIZE);
+
+        if(stream->size == -1) // error
+        {
+            stream->flags |= 2;
+            return EOF;
+        }
+
+        if(stream->size == 0) // eof
+        {
+            stream->flags |= 1;
+            return EOF;
+        }
+    }
+
+    return stream->buf[stream->pos++];
+}
+
+int my_feof(MY_FILE* stream)
+{
+    return stream->flags & 1;
+}
+
+int my_ferror(MY_FILE* stream)
+{
+    return stream->flags & 2;
+}
+
+void my_clearerr(MY_FILE* stream)
+{
+    stream->flags = 0;
+}
+
+MY_FILE* my_fopen(const char *filename, const char *modes)
+{
+    (void)modes; // unused parameter
+
+    int fd = open(filename, O_RDONLY);
+    if(fd == - 1) return NULL;
+
+    MY_FILE *file = calloc(1, sizeof(MY_FILE));
+    file->fd = fd;
+
+    return file;
+}
+
+int my_fclose(MY_FILE* stream)
+{
+    int fd = stream->fd;
+    free(stream);
+    return close(fd) == 0 ? 0 : EOF;
+}
 
 //----------------------------------------------------------------------
 
@@ -33,33 +118,33 @@ struct LineBuffer
  * Return NULL when stream is empty or upon error.
  * Otherwise, return LINEBUFFER.
  */
-struct LineBuffer *readlinebuffer(struct LineBuffer *linebuffer, FILE *stream, char delimiter)
+struct LineBuffer *readlinebuffer(struct LineBuffer *linebuffer, MY_FILE *stream, char delimiter)
 {
     int c;
     char *buffer = linebuffer->buffer;
     char *p = linebuffer->buffer;
     char *end = buffer + linebuffer->size;
 
-    if(feof(stream)) return NULL;
+    if(my_feof(stream)) return NULL;
 
     do
     {
-        c = fgetc(stream);
+        c = my_fgetc(stream);
 
         if(c == EOF)
         {
             // Return NULL when stream is empty or upon error
-            if(p == buffer || ferror(stream)) return NULL;
+            if(p == buffer || my_ferror(stream)) return NULL;
             // append DELIMITER if it's the last line of a file that ends in a character other than DELIMITER.
             if(p[-1] != delimiter) c = delimiter;
         }
 
-        // Check if it's needed to allocate more space
+        // Check if we need to allocate more space
         if(p == end)
         {
             size_t oldsize = linebuffer->size;
-            linebuffer->size += 1024;
-            buffer = realloc(buffer, linebuffer->size); //FIXME
+            linebuffer->size += BUFF_SIZE_INC;
+            buffer = realloc(buffer, linebuffer->size);
             p = buffer + oldsize;
             linebuffer->buffer = buffer;
             end = buffer + linebuffer->size;
@@ -81,7 +166,10 @@ static struct LineBuffer line_buf;
 // printf format string for unnumbered lines.
 static char *print_no_line_fmt = NULL;
 
-// Width of line numbers.
+// Number of blank lines to consider to be one line for numbering (-l).
+static int blank_join = 1;
+
+// Width of line numbers (-w).
 static int lineno_width = 6;
 
 // Current print line number.
@@ -111,10 +199,23 @@ static void print_lineno(void)
 // Process a text line in 'line_buf'.
 static void proc_text(void)
 {
+    static int blank_lines = 0; // Consecutive blank lines so far.
+
     switch(line_type[0])
     {
     case 'a':
-        print_lineno();
+        if (blank_join > 1)
+        {
+            if (line_buf.length > 1 || ++blank_lines == blank_join)
+            {
+                print_lineno();
+                blank_lines = 0;
+            }
+            else
+                fputs(print_no_line_fmt, stdout);
+        }
+        else
+            print_lineno();
         break;
     case 't':
         if(line_buf.length > 1)
@@ -126,7 +227,7 @@ static void proc_text(void)
         fputs(print_no_line_fmt, stdout);
         break;
     case 'p':
-        switch(regexec(&line_regex, line_buf.buffer, 0, NULL, 0)) //FIXME
+        switch(regexec(&line_regex, line_buf.buffer, 0, NULL, 0))
         {
         case 0:
             print_lineno();
@@ -149,25 +250,25 @@ static void proc_text(void)
 }
 
 // Read and process the file pointed to by FP.
-static void process_file(FILE *fp)
+static void process_file(MY_FILE *fp)
 {
     while(readlinebuffer(&line_buf, fp, '\n'))
         proc_text();
 }
 
 // Process file FILE to standard output. Return true if successful.
-static bool nl_file(char const *file)
+bool nl_file(char const *file)
 {
-    FILE *stream;
+    MY_FILE *stream;
 
     if(STREQ(file, "-"))
     {
         have_read_stdin = true;
-        stream = stdin;
+        stream = my_stdin;
     }
     else
     {
-        stream = fopen(file, "r");
+        stream = my_fopen(file, "r");
         if(stream == NULL)
         {
             //fprintf(stderr, "can't open file %s\n", file);
@@ -178,16 +279,16 @@ static bool nl_file(char const *file)
 
     process_file(stream);
 
-    if(ferror(stream))
+    if(my_ferror(stream))
     {
         perror(strerror(errno)); //TODO
         return false;
     }
 
     if(STREQ(file, "-"))
-        clearerr(stream);
+        my_clearerr(stream);
     else
-        if(fclose(stream) == EOF)
+        if(my_fclose(stream) == EOF)
             return false;
 
     return true;
@@ -219,6 +320,7 @@ static bool build_type_arg()
         break;
     default:
         rval = false;
+        fprintf(stderr, "Invalid -b option STYLE: %s\n", optarg);
         break;
     }
     return rval;
@@ -227,8 +329,16 @@ static bool build_type_arg()
 // Parse command arguments
 static bool parse_args(int argc, char **argv)
 {
+    struct option longopts[] = {
+        {"body-numbering", required_argument, NULL, 'b'},
+        {"number-separator", required_argument, NULL, 's'},
+        {"number-width", required_argument, NULL, 'w'},
+        {"join-blank-lines", required_argument, NULL, 'l'},
+        {NULL, 0, NULL, 0}
+    };
+
     int c;
-    while((c = getopt(argc, argv, "b:s:")) != -1)
+    while((c = getopt_long(argc, argv, "b:s:w:l:", longopts, NULL)) != -1)
     {
         switch(c)
         {
@@ -241,6 +351,16 @@ static bool parse_args(int argc, char **argv)
             break;
         case 's':
             separator_str = optarg;
+            break;
+        case 'w':
+            lineno_width = atoi(optarg);
+            break;
+        case 'l':
+            blank_join = atoi(optarg);
+            break;
+        case '?': //TODO
+            fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            return false;
             break;
         default:
             fprintf(stderr, "Usage: %s [-bSTYLE] [-sSTRING] [FILE...]\n", argv[0]);
@@ -263,6 +383,8 @@ int main(int argc, char **argv)
 
     // Initialize the input buffer.
     memset(&line_buf, 0, sizeof(struct LineBuffer));
+    line_buf.size = BUFF_SIZE_INIT;
+    line_buf.buffer = malloc(line_buf.size);
 
     // Initialize the printf format for unnumbered lines.
     size_t len;
@@ -281,7 +403,7 @@ int main(int argc, char **argv)
     // Cleaning
     free(line_buf.buffer);
     if(line_type[0] == 'p') regfree(&line_regex);
-    if(have_read_stdin && fclose(stdin) == EOF) exit(EXIT_FAILURE);
+    if(have_read_stdin && my_fclose(my_stdin) == EOF) exit(EXIT_FAILURE);
 
     exit(ok ? EXIT_SUCCESS : EXIT_FAILURE);
     return EXIT_SUCCESS;
