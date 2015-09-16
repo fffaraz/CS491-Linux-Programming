ffce7e7611d80ad79e1452d905d229a3e944dcb8
Faraz Fallahi <fffaraz@gmail.com>
2015-09-16 17:50:42 -0500

    inc

--- lab5/client/main.c
+++ lab5/client/main.c
@@ -10,7 +10,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
--- lab5/server/main.c
+++ lab5/server/main.c
@@ -12,12 +12,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <string.h>
 #include <sys/select.h>
 #include <sys/sendfile.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
